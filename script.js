81cede9fc095861f93130c3bd4440e908ad05576
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-03-29 22:43:42 -0700

    tweaks

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 var startButton = document.getElementById("start-button");
 var timer = document.getElementById("timer");
 var timeLeft = 75;
+var hasGameStarted = false;
 
 startButton.addEventListener("click", function(){
-    var timerInterval = setInterval(function() {
-       timeLeft--;
-       timer.textContent = "Time Left: " + timeLeft;
-       if (timeLeft <= 0) {
-           clearInterval(timerInterval);
-       } 
-    }, 1000)
+   startTime();
 })
+
+function startTime() {
+    var timerInterval = setInterval(function() {
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+            return;
+        } 
+        timeLeft--;
+       timer.textContent = "Time Left: " + timeLeft;
+       
+    }, 1000)
+}
+
+function startGame () {
+    hasGameStarted = true;
+}
