966efb8cad3721641d95c2cb67ba200b99ae1ad1
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-03-30 14:54:26 -0700

    some css and questions

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
+var startMenuElement = document.querySelector("#start-menu");
+var questionContainerElement = document.querySelector("#question-container");
 var startButton = document.getElementById("start-button");
 var timer = document.getElementById("timer");
 var timeLeft = 75;
 var hasGameStarted = false;
+var questions = [
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        options: ["commas", "quotes", "parentheses", "curly brackets"],
+        answer: "quotes"
+    },
+    
+    {
+        title: "The condition in an if/else statement is enclosed in ____.",
+        options: ["square brackets", "parentheses", "curly brackets", "quotes"],
+        answer: "parentheses"
+    },
+
+    {
+        title: "Commonly used data types DO NOT include:",
+        options: ["booleans", "numbers", "alerts", "strings"],
+        answer: "alerts"
+    },
+
+    {
+        title: "A very useful tool for use during development and debugging for printing content to the debugger is:",
+        options: ["for loops", "JavaScript", "terminal/bash", "console.log"],
+        answer: "console.log"
+    },
+
+    {
+        title: "Arrays in JavaScript can be used to store ____.",
+        options: ["booleans", "numbers and strings", "other arrays", "all of the above"],
+        answer: "all of the above"
+    }
+
+];
 
 startButton.addEventListener("click", function(){
    startTime();
@@ -21,4 +55,6 @@ function startTime() {
 
 function startGame () {
     hasGameStarted = true;
+    startMenuElement.setAttribute("class", "hidden");
+    questionContainerElement.setAttribute("class", "visible");
 }
