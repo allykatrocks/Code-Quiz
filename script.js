b4fa9fadec9765523cfdcea576d0c51b13a3f591
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-03-31 16:53:40 -0700

    main game function

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ var questionContainerElement = document.querySelector("#question-container");
 var startButton = document.getElementById("start-button");
 var timer = document.getElementById("timer");
 var timeLeft = 75;
+var timerInterval;
 var hasGameStarted = false;
+var questionElement = document.querySelector("#questions");
+var answersElement = document.querySelector("#answer-buttons");
+var gameIndex = 0;
 var questions = [
     {
         title: "String values must be enclosed within ____ when being assigned to variables.",
@@ -39,13 +43,15 @@ var questions = [
 
 startButton.addEventListener("click", function(){
    startTime();
+   startGame();
+   startQuestions();
 })
 
 function startTime() {
-    var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function() {
         if (timeLeft <= 0) {
             clearInterval(timerInterval);
-            return;
+            // run end game function
         } 
         timeLeft--;
        timer.textContent = "Time Left: " + timeLeft;
@@ -53,8 +59,45 @@ function startTime() {
     }, 1000)
 }
 
-function startGame () {
+function startGame() {
     hasGameStarted = true;
     startMenuElement.setAttribute("class", "hidden");
     questionContainerElement.setAttribute("class", "visible");
+}
+
+function startQuestions() {
+    questionElement.textContent = questions[gameIndex].title;
+    answersElement.innerHTML = '';
+    for (var i = 0; i < questions[gameIndex].options.length; i++) {
+        var btn = document.createElement("button");
+        btn.textContent = questions[gameIndex].options[i];
+        btn.onclick = checkAnswer;
+        answersElement.append(btn)
+    }
+}
+
+function checkAnswer() {
+    if (this.textContent === questions[gameIndex].answer) {
+        alert("Correct!");
+        // use something else
+    } else {
+        alert("Incorrect!");
+        // use something else
+        timeLeft -= 10;
+    }
+    console.log(this.textContent)
+    console.log("button clicked")
+    gameIndex++;
+    if (gameIndex > 4) {
+        //run end game function
+        clearInterval(timerInterval);
+        console.log("Game over");
+    } else {
+        startQuestions();
+    }
+    
+}
+
+function endGame() {
+    // end the game
 }
